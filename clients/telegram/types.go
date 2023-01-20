57ff6023e2b0703d733ac18dd4d8f593b78519bb
n.li <n.li@ddos-guard.net>
2023-01-20 13:48:37 +0300

    Update telegram types structure

--- clients/telegram/types.go
+++ clients/telegram/types.go
@@ -6,6 +6,21 @@ type UpdatesResponse struct {
 }
 
 type Update struct {
-	ID      int    `json:"update_id"`
-	Message string `json:"message"`
+	ID      int              `json:"update_id"`
+	Message *IncomingMessage `json:"message"`
+}
+
+type IncomingMessage struct {
+	Text string `json:"text"`
+	User User   `json:"from"`
+	Chat Chat   `json:"chat"`
+}
+
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+}
+
+type Chat struct {
+	ID int `json:"id"`
 }
