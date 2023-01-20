78e7289d8dabb6785938fb4e761d077255e23f6d
n.li <n.li@ddos-guard.net>
2023-01-20 11:43:02 +0300

    Added structure for bot events and storage for saving user info

---
+++ storage/storage.go
@@ -0,0 +1,13 @@
+package storage
+
+type Storage interface {
+	Save(u *User) error
+	Remove(u *User) error
+	Get(id string) (User, error)
+	IsExists(u *User) (bool, error)
+}
+
+type User struct {
+	Id       int
+	Username string
+}
